4da0b32a95f68d451c809bf87d562b68b25029fd
Brian Warner <warner@lothar.com>
2018-04-10 21:33:50 -0700

    __main__.py: stop breaking automat-visualize

--- src/wormhole/__main__.py
+++ src/wormhole/__main__.py
@@ -1,6 +1,6 @@
-from wormhole.cli import cli
-
-if __name__ != "__main__":
-    raise ImportError('this module should not be imported')
-
-cli.wormhole()
+if __name__ == "__main__":
+    from wormhole.cli import cli
+    cli.wormhole()
+else:
+    # raise ImportError('this module should not be imported')
+    pass
